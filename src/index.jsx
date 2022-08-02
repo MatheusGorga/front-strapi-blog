d69d56a4757500a2614c5b854bd25faa6d743d2a
MatheusGorga <matheusgobar@gmail.com>
2022-08-02 14:54:33 -0300

    styled

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './templates/App/index';
+import { GlobalStyle } from './styles/global.js';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/themes/theme.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Home />
+    </ThemeProvider>
   </React.StrictMode>,
 );
